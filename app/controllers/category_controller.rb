b00b8a7fc844a26e9a42247dfd3335ae31030570
AR-cogoport <atul.raj@coogoport.com>
2023-02-08 19:00:42 +0530

    testing

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,6 +1,6 @@
 class CategoryController < ApplicationController
     # skip_before_action :verify_authenticity_token
-    before_action :authorize_request, except: :index
+    before_action :authorize_request, except: [:index , :add]
 
     def index
         @category=Category.all
